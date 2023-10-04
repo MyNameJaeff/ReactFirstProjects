05a9b170ad887bf0c4b219969fd9bc57a1973831
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-10-04 14:13:38 +0200

    l

--- React/js/script.js
+++ React/js/script.js
@@ -1,1 +1,15 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+const element = (
+    <main>
+        <img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_960,f_auto/DCTM_Penguin_UK_DK_AL526630_wkmzns.jpg" width="40px"></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintaned by Facebook</li>
+            <li>Powers thoussands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
+//const root = ReactDOM.createRoot(document.getElementById("root"))
+//root.render(element)
